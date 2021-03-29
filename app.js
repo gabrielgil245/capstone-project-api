9748d3b1ad7da2ed3d7e84fbfe20504503320aad
gabrielgil245 <gabrielgil245@gmail.com>
2021-03-28 22:10:31 -0500

    Seeding the Heroku database, categories table.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// require('./lib/seed');
+require('./lib/seed');
 require('./lib/models');
 var cors = require('cors');
 
