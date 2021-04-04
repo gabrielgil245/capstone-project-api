8d803f576c394784c49045be1f214aa3b0ae0074
gabrielgil245 <gabrielgil245@gmail.com>
2021-04-04 00:04:33 -0500

    Get request on User's Id for front-end filter feature.

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const {Category, Question, Answer, User} = require('../lib/models');
 
 // GET /api/v1/profile
+// GET /api/v1/users/me
 // GET /api/v1/categories
 // POST /api/v1/categories/:categoryId/questions - Post question for particular category
 // GET /api/v1/categories/:categoryId/questions - Fetch questions for particular category
@@ -15,7 +16,18 @@ router.get('/profile', async (req, res, next) => {
 
   res.json({
     message:'Established a secure route',
-    user: endUser,
+    userId: endUser.id,
+    token: req.query.token
+  })
+});
+
+router.get('/users/me', async (req, res, next) => {
+  console.log('req.user is', req.user);
+  let endUser = await User.findOne({where: {email: req.user.email}});
+
+  res.json({
+    message:'Established a secure route',
+    userId: endUser.id,
     token: req.query.token
   })
 });
@@ -33,8 +45,16 @@ router.post('/categories/:categoryId/questions', async function(req, res, next) 
 });
 
 router.get('/categories/:categoryId/questions', async function(req, res, next) {
-  let questions = await Question.findAll({where: {categoryId: req.params.categoryId}, include: [{model: Answer}]});
-  res.json(questions);
+  console.log('req.query.userId', req.query.userId);
+  let category = req.params.categoryId;
+  let user = req.query.userId;
+  if(category && user){
+    let questions = await Question.findAll({where: {categoryId: req.params.categoryId, userId: req.query.userId}, include: [{model: Answer}]});
+    res.json(questions);
+  } else {
+    let questions = await Question.findAll({where: {categoryId: req.params.categoryId}, include: [{model: Answer}]});
+    res.json(questions);  
+  }
 });
 
 router.post('/categories/:categoryId/questions/:questionId/answers', async function(req, res, next) {
